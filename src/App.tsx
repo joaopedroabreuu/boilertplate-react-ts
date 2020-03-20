fe5b26caedaaa897a558fe91cba4f3b66c17e729
Joao Pedro Abreu <pedro.abreu@catskillet.com>
2020-03-20 13:29:41 -0300

    Arrumando react-scripts para a versao mais recente

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Provider } from "react-redux";
 import store from "./store";
-import RepositoryList from "./components/RepositoryList";
 import AddressMain from "./components/AddressMain";
 
 const App = () => <Provider store={store}><AddressMain /></Provider>;
